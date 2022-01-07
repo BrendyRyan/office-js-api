f484635253acb18e1cc3e8b018b4e770a57e80f7
Brendan Ryan <brendan.james.ryan@gmail.com>
2022-01-07 17:53:13 -0500

    completed the react add-in tutorial

--- yo-office-react/src/taskpane/components/App.tsx
+++ yo-office-react/src/taskpane/components/App.tsx
@@ -4,7 +4,6 @@ import Header from "./Header";
 import HeroList, { HeroListItem } from "./HeroList";
 import Progress from "./Progress";
 import { tryCatch } from "../lib/utils";
-import {DialogBasicExample} from './Popup'
 
 /* global console, Excel, require  */
 
@@ -19,7 +18,6 @@ export interface AppState {
 
 export default function App(appProps:AppProps){
 const [appState, setAppState] = React.useState([])
-const [popupState, setPopupState] = React.useState(false)
 
 React.useEffect(()=>{
   setAppState([ {
@@ -40,8 +38,7 @@ React.useEffect(()=>{
   },])
 },[])
 
-async function handleClick(){  
-  try {
+async function handleClick(){
     await Excel.run(async (context) => {
       /**
        * Insert your Excel code here
@@ -57,24 +54,21 @@ async function handleClick(){
       await context.sync();
       console.log(`The range address was ${range.address}.`);
     });
-  } catch (error) {
-    console.error(error);
-  }
 };
 
 function createTable() {
-  Excel.run(function (context) {
+  Excel.run(async function (context) {
 
       // TODO1: Queue table creation logic here.
       var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-var expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
-expensesTable.name = "ExpensesTable";
+      var expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
+      expensesTable.name = "ExpensesTable";
 
       // TODO2: Queue commands to populate the table with data.
       expensesTable.getHeaderRowRange().values =
     [["Date", "Merchant", "Category", "Amount"]];
 
-expensesTable.rows.add(null /*add at the end*/, [
+  expensesTable.rows.add(null /*add at the end*/, [
     ["1/1/2017", "The Phone Company", "Communications", "120"],
     ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
     ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
@@ -86,70 +80,52 @@ expensesTable.rows.add(null /*add at the end*/, [
 
       // TODO3: Queue commands to format the table.
       expensesTable.columns.getItemAt(3).getRange().numberFormat = [['\u20AC#,##0.00']];
-expensesTable.getRange().format.autofitColumns();
-expensesTable.getRange().format.autofitRows();
+    expensesTable.getRange().format.autofitColumns();
+    expensesTable.getRange().format.autofitRows();
 
-      return context.sync();
+      await context.sync();
   })
-  .catch(function (error) {
-      console.log("Error: " + error);
-      if (error instanceof OfficeExtension.Error) {
-          console.log("Debug info: " + JSON.stringify(error.debugInfo));
-      }
-  });
 }
 
 function filterTable() {
-  Excel.run(function (context) {
+  Excel.run(async function (context) {
 
       // TODO1: Queue commands to filter out all expense categories except Groceries and Education.
       var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
-var categoryFilter = expensesTable.columns.getItem('Category').filter;
-categoryFilter.applyValuesFilter(['Education', 'Groceries']);
+      var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+      var categoryFilter = expensesTable.columns.getItem('Category').filter;
+      categoryFilter.applyValuesFilter(['Education', 'Groceries']);
 
-      return context.sync();
+      await context.sync();
   })
-  .catch(function (error) {
-      console.log("Error: " + error);
-      if (error instanceof OfficeExtension.Error) {
-          console.log("Debug info: " + JSON.stringify(error.debugInfo));
-      }
-  });
 }
 
 function sortTable() {
-  Excel.run(function (context) {
+  Excel.run(async function (context) {
 
       // TODO1: Queue commands to sort the table by Merchant name.
       var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
-var sortFields = [
+      var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+    var sortFields = [
     {
         key: 1,            // Merchant column
         ascending: false,
     }
 ];
 
-expensesTable.sort.apply(sortFields);
+    expensesTable.sort.apply(sortFields);
 
-      return context.sync();
+      await context.sync();
   })
-  .catch(function (error) {
-      console.log("Error: " + error);
-      if (error instanceof OfficeExtension.Error) {
-          console.log("Debug info: " + JSON.stringify(error.debugInfo));
-      }
-  });
 }
 
 function createChart() {
-  Excel.run(function (context) {
+  Excel.run(async function (context) {
 
       // TODO1: Queue commands to get the range of data to be charted.
       var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
-var dataRange = expensesTable.getDataBodyRange();
+    var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+    var dataRange = expensesTable.getDataBodyRange();
 
       // TODO2: Queue command to create the chart and define its type.
       var chart = currentWorksheet.charts.add('ColumnClustered', dataRange, 'Auto');
@@ -163,35 +139,41 @@ var dataRange = expensesTable.getDataBodyRange();
       chart.dataLabels.format.font.color = "black";
       chart.series.getItemAt(0).name = 'Value in \u20AC';
 
-      return context.sync();
+      await context.sync();
   })
-  .catch(function (error) {
-      console.log("Error: " + error);
-      if (error instanceof OfficeExtension.Error) {
-          console.log("Debug info: " + JSON.stringify(error.debugInfo));
-      }
-  });
 }
 
 function freezeHeader() {
-  Excel.run(function (context) {
+  Excel.run(async function (context) {
 
       // TODO1: Queue commands to keep the header visible when the user scrolls.
       var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-currentWorksheet.freezePanes.freezeRows(1);
+    currentWorksheet.freezePanes.freezeRows(1);
 
-      return context.sync();
+      await context.sync();
   })
-  .catch(function (error) {
-      console.log("Error: " + error);
-      if (error instanceof OfficeExtension.Error) {
-          console.log("Debug info: " + JSON.stringify(error.debugInfo));
-      }
-  });
 }
 
+let dialog = null;
+
 function triggerPopup(){
-setPopupState(!popupState)
+  // TODO1: Call the Office Common API that opens a dialog
+  Office.context.ui.displayDialogAsync(
+    'https://localhost:3000/popup.html',
+    {height: 45, width: 55},
+
+    // TODO2: Add callback parameter.
+    function (result) {
+      dialog = result.value;
+      dialog.addEventHandler(Office.EventType.DialogMessageReceived, processMessage);
+  }
+);
+
+}
+
+function processMessage(arg) {
+  document.getElementById("user-name").innerHTML = arg.message;
+  dialog.close();
 }
 
 const { title, isOfficeInitialized } = appProps;
@@ -206,12 +188,6 @@ if (!isOfficeInitialized) {
     );
   }
 
-  if (popupState === true){
-    return(
-      <DialogBasicExample />
-    )
-  }
-
   return (
     <div className="ms-welcome">
       <Header logo={require("./../../../assets/logo-filled.png")} title={appProps.title} message="Welcome" />
@@ -219,27 +195,32 @@ if (!isOfficeInitialized) {
         <p className="ms-font-l">
           Modify the source files, then click <b>Run</b>.
         </p>
-        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={handleClick}>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={()=>tryCatch(handleClick)}>
           Run
         </DefaultButton>
-        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={createTable}>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={()=>tryCatch(createTable)}>
           Create Table
         </DefaultButton>
-        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={filterTable}>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={()=>tryCatch(filterTable)}>
           Filter Table
         </DefaultButton>
-        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={sortTable}>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={()=>tryCatch(sortTable)}>
           Sort Table
         </DefaultButton>
-        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={createChart}>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={()=>tryCatch(createChart)}>
           Create Chart
         </DefaultButton>
-        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={freezeHeader}>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={()=>tryCatch(freezeHeader)}>
           Freeze Header
         </DefaultButton>
         <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={triggerPopup}>
           Open Popup
         </DefaultButton>
+        <br/>
+        <br/>
+        <label htmlFor="user-name" id="user-name"></label>
+        <br />
+        <br />
       </HeroList>
     </div>
   );
